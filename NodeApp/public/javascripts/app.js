5b383e8772802e509096f5214c48a5b5798dc73f
Bhavna Saluja <bhavnasaluja@vagelos-ve501-0197.apn.wlan.upenn.edu>
2018-12-08 18:23:24 -0500

    Queries 1 and 8 -
    Show the patents owned by a child company (aka. acquired) or a parent company.  E.g. choose a child or parent from a dropdown menu.
    What are all the patents that fall in a layman category (or technology) which the parent company owns (e.g. combining CPC subgroups into “AI”, “machine learning”)?

--- NodeApp/public/javascripts/app.js
+++ NodeApp/public/javascripts/app.js
@@ -118,4 +118,118 @@ app.controller('showfamilyController', function ($scope,$http){
 
     };
 
+    $scope.Submit = function(parentCompany,childCompany){
+        console.log(parentCompany);
+        console.log(childCompany);
+        var request = $http.get('/query1_bs/parent/'+parentCompany+'/child/'+childCompany);
+        request.success(function(data){
+            console.log('got response');
+            $scope.data = data;
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+
+    };
+
+});
+
+
+//show family
+app.controller('familyDropDownController', function($scope, $http) {
+    $scope.message="";
+    $scope.selectedLogin = null;
+    $scope.logins = [];
+    var request = $http.get('/familyData');
+    request.success(function(data) {
+        $scope.logins = data;
+    });
+    request.error(function(data){
+        console.log('err');
+    });
+    $scope.selectedLogin = $scope.logins[0];
+
+    $scope.ShowFamily = function() {
+        console.log('abcdefgh');
+        console.log($scope.selectedLogin);
+        var request = $http.get('/family/'+ $scope.selectedLogin.login);
+        request.success(function(data) {
+            $scope.data = data;
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    }; 
+});
+
+//show friends
+app.controller('friendsController', function($scope, $http) {
+        $scope.message="";
+        $scope.ShowFriends = function() {
+        var request = $http.get('/friends/'+$scope.login);
+        request.success(function(data) {
+            $scope.data = data;
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    
+    }; 
+});
+
+
+app.controller('query2_bscontroller', function ($scope,$http){
+    var familyList = [];
+    console.log('aaa');
+    var request = $http.get('/getFamily');
+            request.success(function(data){
+            console.log('got response');
+            var parentList = [];
+            //console.log(data);
+            for (x =0;x<data.length;x++)
+            {
+                familyList.push(data[x]);
+                parentList.push(data[x].ParentCompany)
+            }
+            $scope.familymembers = familyList;
+            parentUniqueList = parentList.filter(onlyUnique);
+            $scope.parentUniqueList = parentUniqueList;
+
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+
+    $scope.Change = function(parentItem){
+
+        childrenList = [];
+        console.log("entered");
+        if('Google' === parentItem)
+        {
+            childrenList.push('Artificial Intelligence');
+            childrenList.push('Autonomous Driving');
+            childrenList.push('Search Engine');
+        }
+        else if ('Microsoft' === parentItem) {
+            childrenList.push('Autonomous Driving');
+            childrenList.push('Search Engine');
+        } 
+
+        $scope.childrenList = childrenList;
+    };
+
+    $scope.Submit = function(parentCompany,childCompany){
+        console.log(parentCompany);
+        console.log(childCompany);
+        var request = $http.get('/query2_bs/parent/'+parentCompany+'/child/'+childCompany);
+        request.success(function(data){
+            console.log('got response');
+            $scope.data = data;
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+
+    };
+
 });
